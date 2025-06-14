3babab4ecd6167f802b0762bdb7a19aebb25ad8c
OhKannaDuh <uimokd@gmail.com>
2025-06-14 16:25:23 +0100

    Allow max attempts in RetryChainFactory

--- Ocelot/Chain/RetryChainFactory.cs
+++ Ocelot/Chain/RetryChainFactory.cs
@@ -6,22 +6,38 @@ namespace Ocelot.Chain;
 
 public abstract class RetryChainFactory : ChainFactory
 {
+    private int attempt = 0;
+
     public override Func<Chain> Factory()
     {
         var name = GetType().Name;
+
         return () => Chain.Create(name)
             .Then(new TaskManagerTask(() =>
             {
+                if (IsComplete())
+                {
+                    return true;
+                }
+
+                if (attempt >= GetMaxAttempts())
+                {
+                    Logger.Debug($"{name}: Max retry attempts reached.");
+                    return true;
+                }
+
                 var watcher = ChainManager.Get($"{name}##watcher");
                 if (!watcher.IsRunning)
                 {
                     if (EzThrottler.Throttle(name, GetThrottle()))
                     {
+                        attempt++;
+                        Logger.Debug($"{name}: Attempt {attempt}/{GetMaxAttempts()}");
                         watcher.Submit(() => Create(Chain.Create(name)));
                     }
                 }
 
-                return IsComplete();
+                return false;
             }, new() { TimeLimitMS = GetTimeout() }));
     }
 
@@ -30,6 +46,8 @@ public abstract class RetryChainFactory : ChainFactory
     public virtual int GetThrottle() => 500;
 
     public virtual int GetTimeout() => 10000;
+
+    public virtual int GetMaxAttempts() => int.MaxValue;
 
     public override TaskManagerConfiguration? Config() => new()
     {
